7439f2dbaf12d74c83b8d50a2f18ebc59b53000a
CoderBdk <cobdgithub07@gmail.com>
2026-07-07 10:53:09 +0600

    feat(ui): enhance BottomBar and add center action
    - Add a new center action button

--- app/src/main/java/com/coderbdk/budgetbuddy/ui/main/BudgetBuddyApp.kt
+++ app/src/main/java/com/coderbdk/budgetbuddy/ui/main/BudgetBuddyApp.kt
@@ -1,34 +1,52 @@
 package com.coderbdk.budgetbuddy.ui.main
 
+import android.R.attr.contentDescription
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCard
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Analytics
 import androidx.compose.material.icons.filled.BusinessCenter
+import androidx.compose.material.icons.filled.CenterFocusStrong
+import androidx.compose.material.icons.filled.DynamicFeed
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Print
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Analytics
 import androidx.compose.material.icons.outlined.BusinessCenter
+import androidx.compose.material.icons.outlined.CenterFocusStrong
+import androidx.compose.material.icons.outlined.DynamicFeed
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -36,8 +54,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -49,7 +73,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
@@ -66,13 +90,12 @@ import com.coderbdk.budgetbuddy.ui.navigation.getNavDestinationTitle
 import com.coderbdk.budgetbuddy.ui.settings.SettingsViewModel
 import com.coderbdk.budgetbuddy.ui.theme.BudgetBuddyTheme
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BudgetBuddyApp() {
     val navController = rememberNavController()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val topAppBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    // val topAppBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     val currentDestination = navBackStackEntry?.destination
 
 
@@ -84,6 +107,12 @@ fun BudgetBuddyApp() {
                 "Budgets",
                 Icons.Filled.BusinessCenter,
                 Icons.Outlined.BusinessCenter
+            ),
+            BottomNavItem(
+                Screen.Analytics,
+                "Dynamic",
+                Icons.Filled.CenterFocusStrong,
+                Icons.Outlined.CenterFocusStrong
             ),
             BottomNavItem(
                 Screen.Analytics,
@@ -105,7 +134,7 @@ fun BudgetBuddyApp() {
 
     val mainViewModel = hiltViewModel<MainViewModel>()
     val settingsViewModel = hiltViewModel<SettingsViewModel>()
-    val fabVisible by mainViewModel.fabVisible.collectAsState()
+/*    val fabVisible by mainViewModel.fabVisible.collectAsState()
 
     val nestedScrollConnection = remember {
         object : NestedScrollConnection {
@@ -114,7 +143,8 @@ fun BudgetBuddyApp() {
                 return Offset.Zero
             }
         }
-    }
+    }*/
+
     val currentRouteMetadata = currentDestination?.getNavDestinationMetadata()
     val title = currentRouteMetadata?.getNavDestinationTitle(R.string.app_name)?.let { stringResource(it) }
 
@@ -124,8 +154,9 @@ fun BudgetBuddyApp() {
         Scaffold(
             modifier = Modifier
                 .fillMaxSize()
-                .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
-                .nestedScroll(nestedScrollConnection),
+                //.nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
+                //.nestedScroll(nestedScrollConnection)
+                    ,
             topBar = {
                 TopAppBar(
                     title = {
@@ -147,7 +178,19 @@ fun BudgetBuddyApp() {
                             }
                         }
                     },
-                    scrollBehavior = topAppBarScrollBehavior
+                    actions = {
+                        if(currentDestination?.hasRoute(Screen.Home::class) == true) {
+                            FilledTonalIconButton (
+                                onClick = {}
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Notifications,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                    },
+                   // scrollBehavior = topAppBarScrollBehavior
                 )
             },
             bottomBar = {
@@ -155,46 +198,29 @@ fun BudgetBuddyApp() {
                     currentDestination,
                     bottomNavItems,
                     onNavigate = {
-                        navController.navigate(it.route) {
+                        navController.navigate(it) {
                             popUpTo(navController.graph.findStartDestination().id) {
                                 saveState = true
                             }
                             launchSingleTop = true
                             restoreState = true
                         }
-                    }
+                    },
+                    onPerformFabAction = mainViewModel::performFabAction
                 )
-                /*NavigationBar {
-                    bottomNavItems.forEach { item ->
-                        NavigationBarItem(
-                            icon = { Icon(item.icon, contentDescription = item.label) },
-                            label = { Text(item.label) },
-                            selected = currentDestination?.hierarchy?.any { it.hasRoute(item.route::class) } == true,
-                            onClick = {
-                                navController.navigate(item.route) {
-                                    popUpTo(navController.graph.findStartDestination().id) {
-                                        saveState = true
-                                    }
-                                    launchSingleTop = true
-                                    restoreState = true
-                                }
-                            }
-                        )
-                    }
-                }*/
             },
-            floatingActionButton = {
+           /* floatingActionButton = {
                 AnimatedVisibility(
-                    visible = /*currentDestination?.hasRoute(AddTransaction::class) != true && currentDestination?.hasRoute(
+                    visible = *//*currentDestination?.hasRoute(AddTransaction::class) != true && currentDestination?.hasRoute(
                         Budget::class
-                    ) != true &&*/ fabVisible,
+                    ) != true &&*//* fabVisible,
                     enter = slideInVertically() + fadeIn(),
                     exit = slideOutVertically() + fadeOut()
                 ) {
                     val baseRoute = currentDestination?.route?.substringBefore("/") ?: ""
-                    FloatingActionButtonContent(baseRoute, navController, mainViewModel)
+                    FloatingActionButtonContent(baseRoute, navController::navigate, mainViewModel::performFabAction)
                 }
-            }) { innerPadding ->
+            }*/) { innerPadding ->
             BudgetBuddyNavGraph(
                 navController = navController,
                 mainViewModel = mainViewModel,
@@ -206,38 +232,55 @@ fun BudgetBuddyApp() {
 }
 
 @Composable
-fun FloatingActionButtonContent(
-    baseRoute: String,
-    navController: NavController,
-    mainViewModel: MainViewModel
+fun CenterBottomBarAction(
+    baseRoute: String?,
+    onNavigate: (Screen) -> Unit,
+    onPerformAction: (FabAction) -> Unit
 ) {
+
+    val isEnabled = baseRoute in listOf(
+        Screen.Home::class.qualifiedName,
+        Screen.Budgets::class.qualifiedName,
+        Screen.Transactions::class.qualifiedName
+    )
+
+    var lastIcon by remember {
+        mutableStateOf(Icons.Default.Add)
+    }
+    var lastDescription by rememberSaveable {
+        mutableStateOf("Add Transaction")
+    }
+
     when (baseRoute) {
         Screen.Home::class.qualifiedName -> {
-            FloatingActionButton(onClick = {
-                navController.navigate(Screen.AddTransaction)
-            }) {
-
-                Icon(
-                    Icons.Default.AddCircle,
-                    contentDescription = "Add Transaction"
-                )
-            }
+            lastIcon = Icons.Default.Add
+            lastDescription = "Add Transaction"
         }
-
         Screen.Budgets::class.qualifiedName -> {
-            FloatingActionButton(onClick = {
-                mainViewModel.performFabAction(FabAction.AddBudget)
-            }) {
-                Icon(Icons.Default.Add, contentDescription = "Add Budget")
-            }
+            lastIcon = Icons.Default.Add
+            lastDescription = "Add Budget"
         }
         Screen.Transactions::class.qualifiedName -> {
-            FloatingActionButton(onClick = {
-                mainViewModel.performFabAction(FabAction.PrintTransaction)
-            }) {
-                Icon(Icons.Default.Print, contentDescription = "Print Transaction")
-            }
+            lastIcon = Icons.Default.Print
+            lastDescription = "Print Transaction"
         }
+    }
+
+    FilledTonalIconButton(
+        onClick = {
+            when (baseRoute) {
+                Screen.Home::class.qualifiedName -> onNavigate(Screen.AddTransaction)
+                Screen.Budgets::class.qualifiedName -> onPerformAction(FabAction.AddBudget)
+                Screen.Transactions::class.qualifiedName -> onPerformAction(FabAction.PrintTransaction)
+            }
+        },
+        enabled = isEnabled,
+        shape = CircleShape,
+    ) {
+        Icon(
+            imageVector = lastIcon,
+            contentDescription = lastDescription
+        )
     }
 }
 
@@ -254,12 +297,19 @@ private data class BottomNavItem<T : Any>(
 private fun BudgetBuddyBottomNavigation(
     currentDestination: NavDestination?,
     bottomNavItems: List<BottomNavItem<out Screen>>,
-    onNavigate: (BottomNavItem<out Screen>) -> Unit
+    onNavigate: (Screen) -> Unit,
+    onPerformFabAction: (FabAction) -> Unit
 ) {
     NavigationBar(
         containerColor = Color.Transparent
     ) {
         bottomNavItems.forEach { item ->
+
+            if(item.label == "Dynamic") {
+                val baseRoute = currentDestination?.route?.substringBefore("/") ?: ""
+                CenterBottomBarAction(baseRoute, onNavigate, onPerformFabAction)
+                return@forEach
+            }
             val isSelected =
                 currentDestination?.hierarchy?.any { it.hasRoute(item.route::class) } == true
             NavigationBarItem(
@@ -273,8 +323,11 @@ private fun BudgetBuddyBottomNavigation(
                 label = { Text(item.label) },
                 selected = isSelected,
                 onClick = {
-                    onNavigate(item)
+                    onNavigate(item.route)
                 },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Color.Transparent
+                )
             )
         }
     }
@@ -298,6 +351,12 @@ private fun AppPreview() {
             ),
             BottomNavItem(
                 Screen.Analytics,
+                "Dynamic",
+                Icons.Filled.CenterFocusStrong,
+                Icons.Outlined.CenterFocusStrong
+            ),
+            BottomNavItem(
+                Screen.Analytics,
                 "Analytics",
                 Icons.Filled.Analytics,
                 Icons.Outlined.Analytics
@@ -316,7 +375,8 @@ private fun AppPreview() {
             bottomBar = {
                 BudgetBuddyBottomNavigation(
                     currentDestination = currentDestination,
-                    bottomNavItems
+                    bottomNavItems,
+                    onNavigate = {}
                 ) { }
             }
         ) { innerPadding ->
